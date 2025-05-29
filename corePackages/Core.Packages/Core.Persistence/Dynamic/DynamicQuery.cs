5507db40a5ff778469d8d7e3c631a2d13769c69d
yildirimogcod <yildirimogcod@gmail.com>
2025-05-29 20:52:52 +0300

    core.packages oluşturuldu

---
+++ corePackages/Core.Packages/Core.Persistence/Dynamic/DynamicQuery.cs
@@ -0,0 +1,18 @@
+﻿namespace Core.Persistence.Dynamic
+{
+    public class DynamicQuery
+    {
+        public Filter? Filter { get; set; }
+        public IEnumerable<Sort> Sort { get; set; }
+        public DynamicQuery()
+        {
+            
+        }
+        public DynamicQuery(Filter? filters, IEnumerable<Sort>? sort)
+        {
+            Filter = filters;
+            Sort = sort;
+          
+        }
+    }
+}
---
+++ corePackages/Core.Packages/Core.Persistence/Dynamic/Sort.cs
@@ -0,0 +1,21 @@
+﻿namespace Core.Persistence.Dynamic
+{
+    public class Sort
+    {
+        public string Field { get; set; }
+        
+        public string Dir { get; set; }
+
+
+        public Sort()
+        {
+            Field = string.Empty;
+            Dir = string.Empty;
+        }
+        public Sort(string field, string direction)
+        {
+            Field = field;
+            Dir = direction;
+        }
+    }
+}
